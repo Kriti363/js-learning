77d5848d30a262b71312fae35009769e508be53f
Kriti363 <srivastavakriti171024@gmail.com>
2026-09-01 16:50:45 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -8,6 +8,7 @@ console.log(2 != 1);
 
 console.log("2" > 1);      // true
 console.log("02" > 1);    //  true
+console.log("03" > 1);
 
 console.log(null > 0);        // false
 console.log(null == 0);      //  false
